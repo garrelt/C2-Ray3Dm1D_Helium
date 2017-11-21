9474617896b9981a3af4f774423c60839b199105
Garrelt Mellema <garrelt@astro.su.se>
2017-11-21 11:24:58 +0100

    Uncommitted files found when returning to code for multiphase project. editing seems to be cosmetic for sourceprops_cubep3m.F90 and practical for Makefile

--- code/files_for_3D/sourceprops_cubep3m.F90
+++ code/files_for_3D/sourceprops_cubep3m.F90
@@ -26,8 +26,8 @@ module sourceprops
 #endif
 #ifdef QUASARS
   use c2ray_parameters, only: qpl_S_star_nominal,qEddLeff_nominal,&
-      qmass_nominal,qEddLum, qpl_index_nominal, qpl_MinFreq_nominal,&
-      qpl_MaxFreq_nominal
+       qmass_nominal,qEddLum, qpl_index_nominal, qpl_MinFreq_nominal,&
+       qpl_MaxFreq_nominal
 #endif
   use c2ray_parameters, only: phot_per_atom, lifetime, S_star_nominal,&
        StillNeutral, Number_Sourcetypes
@@ -78,8 +78,8 @@ module sourceprops
   real(kind=dp),dimension(:),allocatable :: NormFluxPL !< normalized ionizing flux of sources
 #endif
 #ifdef QUASARS
- real(kind=dp),dimension(:),allocatable :: NormFluxQPL !< normalized ionizing flux of sources
- integer,private :: NumQsr !< counter: number of quasar sources
+  real(kind=dp),dimension(:),allocatable :: NormFluxQPL !< normalized ionizing flux of sources
+  integer,private :: NumQsr !< counter: number of quasar sources
 #endif
   integer,dimension(:),allocatable :: srcSeries  !< a randomized list of sources
   real(kind=dp),dimension(:),allocatable :: uv_array  !< list of UV flux evolution (for some sources models)
@@ -102,29 +102,29 @@ module sourceprops
   character(len=512),private :: sourcelistfile,sourcelistfilesuppress
 
 #ifdef MPI
-    integer :: mympierror
+  integer :: mympierror
 #endif
- 
+
 contains
-  
+
   ! =======================================================================
 
   !> Set the source properties for this redshift
   subroutine source_properties(zred_now,nz,lifetime2,restart)
-    
+
     ! Input routine: establish the source properties
-    
+
     real(kind=dp),intent(in) :: zred_now ! current redshift
     real(kind=dp),intent(in) :: lifetime2 ! time step
     integer,intent(in) :: nz
     integer,intent(in) :: restart
-    
+
     integer :: ns,ns0
-    
+
 #ifdef MPILOG     
     write(logf,*) "Check sourceprops: ",zred_now,nz,lifetime2,restart
 #endif 
-    
+
     ! Deallocate source arrays
     if (allocated(srcpos)) deallocate(srcpos)
     if (allocated(srcMass)) deallocate(srcMass)
@@ -142,14 +142,14 @@ contains
 
     ! Save previous number of sources
     Prev_NumSrc=NumSrc
-    
+
     ! Rank 0 reads in sources
     if (rank == 0) then
-       
+
        ! Sources are read from files with redshift in the file name:
        ! construct redshift string
        write(z_str,'(f6.3)') zred_now
-       
+
        ! Construct the file names
        sourcelistfile=trim(adjustl(dir_src))//&       
             trim(adjustl(z_str))//"-"//trim(adjustl(id_str))// &
@@ -157,20 +157,20 @@ contains
        sourcelistfilesuppress=trim(adjustl(dir_src))//&
             trim(adjustl(z_str))//"-"//trim(adjustl(id_str))// &
             trim(adjustl(sourcelistfilesuppress_base))
-       
+
        call establish_number_of_active_sources (restart)
-       
+
     endif ! end of rank 0 test
-    
+
 #ifdef MPI
     ! Distribute source number to all other nodes
     call MPI_BCAST(NumSrc,1,MPI_INTEGER,0,MPI_COMM_NEW,mympierror)
 #endif
-    
+
 #ifdef MPILOG
     if (rank /=0) write(logf,*) "Number of sources, with suppression: ",NumSrc
 #endif
-    
+
     ! Allocate arrays for this NumSrc
     if (NumSrc > 0) then
        allocate(srcpos(3,NumSrc))
@@ -190,14 +190,14 @@ contains
        ! Fill in the source arrays
        if (rank == 0) then
           call read_in_sources (restart)
-          
+
           ! Set cumulative number of uv photons to zero if this is not the
           ! first redshift for which sources are active (this way cumulative_uv
           ! can be used in all cases).
           ! New version: cumulative_uv is slowly reduced
           !if (Prev_NumSrc /= 0) cumulative_uv=0.0
           call assign_uv_luminosities (lifetime2,nz)
-          
+
           write(logf,*) 'Source lifetime=', lifetime2/(1e6*YEAR),' Myr'
           write(logf,*) 'Total photon rate (BB)= ', &
                sum(NormFlux)*S_star_nominal,' s^-1'
@@ -209,7 +209,7 @@ contains
           write(logf,*) 'Total photon rate (Q)= ', &
                sum(NormFluxQPL)*qpl_S_star_nominal,' s^-1'
 #endif
-          
+
        endif ! of rank 0 test
 
 #ifdef MPI
@@ -229,19 +229,19 @@ contains
 
 #endif
 #endif
-    
+
        ! Randomize the source list
        !call create_random_source_list()
 
     else
-    
+
        ! For the prescribed uv evolution model, accumulate the photons
        ! (to be used once the first sources appear)
        if (UV_Model == "Fixed N_gamma") &
             cumulative_uv=cumulative_uv + uv_array(nz)
-       
+
     endif
-    
+
   end subroutine source_properties
 
   ! =======================================================================
@@ -249,9 +249,9 @@ contains
   subroutine establish_number_of_active_sources (restart)
 
     integer,intent(in) :: restart
-    
+
     real :: odens	
-    
+
     integer :: ns0
     integer :: ns
 
@@ -260,11 +260,11 @@ contains
        open(unit=50,file=sourcelistfile,status='old')
        ! Number of sources
        read(50,*) NumSrc0
-       
+
        ! Report
        write(logf,*) "Total number of source locations, no suppression: ", &
             NumSrc0
-       
+
        ! Read in source positions and mass to count the number
        ! of non-suppressed sources
        NumSrc = 0
@@ -281,26 +281,27 @@ contains
 #ifdef QUASARS
           if (srclist(HMACH) /= 0.0 .or. srclist(QSO) /= 0.0) then
 #else
-          if (srclist(HMACH) /= 0.0) then
+             if (srclist(HMACH) /= 0.0) then
 #endif
-             NumSrc=NumSrc+1
-          ! if the cell is still neutral, no suppression (if we use the Iliev
-          ! et al source model)   
-          elseif (xh(srcpos0(1),srcpos0(2),srcpos0(3),1) < StillNeutral .and. &
-               UV_Model == "Iliev et al") then
-             NumSrc=NumSrc+1
-          endif
+                NumSrc=NumSrc+1
+                ! if the cell is still neutral, no suppression (if we use the Iliev
+                ! et al source model)   
+             elseif (xh(srcpos0(1),srcpos0(2),srcpos0(3),1) < StillNeutral .and. &
+                  UV_Model == "Iliev et al") then
+                NumSrc=NumSrc+1
+             endif
 
-          ! Count different types of sources
-          if (srclist(HMACH) /= 0.0) NumMassiveSrc=NumMassiveSrc+1
-          if (srclist(LMACH) /= 0.0) NumSupprbleSrc=NumSupprbleSrc+1
+             ! Count different types of sources
+             if (srclist(HMACH) /= 0.0) NumMassiveSrc=NumMassiveSrc+1
+             if (srclist(LMACH) /= 0.0) NumSupprbleSrc=NumSupprbleSrc+1
 #ifdef QUASARS
-          if (srclist(QSO) /= 0.0) NumQsr = NumQsr+1 
+             if (srclist(QSO) /= 0.0) NumQsr = NumQsr+1 
 #endif
-          ! How many suppressed?
-          if (srclist(LMACH) /= 0.0) then
-             if (xh(srcpos0(1),srcpos0(2),srcpos0(3),1) > StillNeutral .or. &
-               UV_Model /= "Iliev et al") NumSupprsdSrc=NumSupprsdSrc+1
+             ! How many suppressed?
+             if (srclist(LMACH) /= 0.0) then
+                if (xh(srcpos0(1),srcpos0(2),srcpos0(3),1) > StillNeutral .or. &
+                     UV_Model /= "Iliev et al") NumSupprsdSrc=NumSupprsdSrc+1
+             endif
           endif
        enddo
        close(50)
@@ -351,49 +352,49 @@ contains
           if (xh(srcpos0(1),srcpos0(2),srcpos0(3),1) < StillNeutral) then
 #ifdef QUASARS
              if (UV_Model == "Iliev et al" .or. srclist(HMACH) > 0.0d0 .or. &
-                 srclist(QSO) > 0.0d0) then
+                  srclist(QSO) > 0.0d0) then
 #else
-             if (UV_Model == "Iliev et al" .or. srclist(HMACH) > 0.0d0) then
+                if (UV_Model == "Iliev et al" .or. srclist(HMACH) > 0.0d0) then
 #endif
-                ! the cell is still neutral, no suppression
+                   ! the cell is still neutral, no suppression
+                   ns=ns+1
+                   ! Source positions in file start at 1!
+                   srcpos(1,ns)=srcpos0(1)
+                   srcpos(2,ns)=srcpos0(2)
+                   srcpos(3,ns)=srcpos0(3)
+                   SrcMass(ns,1)=srclist(HMACH)
+                   if (UV_Model == "Iliev et al") then
+                      SrcMass(ns,2)=srclist(LMACH)
+                   else
+                      SrcMass(ns,2)=0.0
+                   endif
+#ifdef QUASARS
+                   NormFluxQPL(ns)=srclist(QSO) ! set QSO source flux 
+                   ! (here it is assumed they are always active)
+#endif
+                endif
+#ifdef QUASARS
+             elseif (srclist(HMACH) > 0.0d0 .or. srclist(QSO) > 0.0d0) then
+#else
+             elseif (srclist(HMACH) > 0.0d0) then
+#endif
+                !the cell is ionized but source is massive enough to survive
+                !and is assumed Pop. II 
                 ns=ns+1
                 ! Source positions in file start at 1!
                 srcpos(1,ns)=srcpos0(1)
                 srcpos(2,ns)=srcpos0(2)
                 srcpos(3,ns)=srcpos0(3)
                 SrcMass(ns,1)=srclist(HMACH)
-                if (UV_Model == "Iliev et al") then
-                   SrcMass(ns,2)=srclist(LMACH)
-                else
-                   SrcMass(ns,2)=0.0
-                endif
+                SrcMass(ns,2)=0.0
 #ifdef QUASARS
                 NormFluxQPL(ns)=srclist(QSO) ! set QSO source flux 
                 ! (here it is assumed they are always active)
 #endif
              endif
-#ifdef QUASARS
-          elseif (srclist(HMACH) > 0.0d0 .or. srclist(QSO) > 0.0d0) then
-#else
-          elseif (srclist(HMACH) > 0.0d0) then
-#endif
-             !the cell is ionized but source is massive enough to survive
-             !and is assumed Pop. II 
-             ns=ns+1
-             ! Source positions in file start at 1!
-             srcpos(1,ns)=srcpos0(1)
-             srcpos(2,ns)=srcpos0(2)
-             srcpos(3,ns)=srcpos0(3)
-             SrcMass(ns,1)=srclist(HMACH)
-             SrcMass(ns,2)=0.0
-#ifdef QUASARS
-             NormFluxQPL(ns)=srclist(QSO) ! set QSO source flux 
-             ! (here it is assumed they are always active)
-#endif
           endif
-
        enddo
-       
+
        ! Collect total source mass (weigthed with efficiency factor
        ! in case of the Iliev et al source model).
        if (UV_Model == "Iliev et al") then
@@ -423,24 +424,24 @@ contains
        ! Data in saved source list depends on what sources were
        ! recorded / are being used.
 #if defined(QUASARS) && defined(PL)
-         if (allocated(temparray)) deallocate(temparray)
-          allocate(temparray(3))
+       if (allocated(temparray)) deallocate(temparray)
+       allocate(temparray(3))
 #elif defined(QUASARS)
-          if (allocated(temparray)) deallocate(temparray)
-          allocate(temparray(2))   
+       if (allocated(temparray)) deallocate(temparray)
+       allocate(temparray(2))   
 #elif defined(PL)
-          if (allocated(temparray)) deallocate(temparray)
-          allocate(temparray(2))
+       if (allocated(temparray)) deallocate(temparray)
+       allocate(temparray(2))
 #else
-          if (allocated(temparray)) deallocate(temparray)
-          allocate(temparray(1))
+       if (allocated(temparray)) deallocate(temparray)
+       allocate(temparray(1))
 #endif
 
        do ns0=1,NumSrc
-          
+
           read(49,*) srcpos(1,ns0),srcpos(2,ns0),srcpos(3,ns0),&
                temparray
-          
+
           SrcMass(ns0,0) = temparray(1)
 #if defined(QUASARS) && defined(PL)
           NormFluxPL(ns0) = temparray(2)
@@ -454,7 +455,7 @@ contains
        deallocate(temparray)
        close(49)
     endif ! of restart test
-    
+
   end subroutine read_in_sources
 
   ! =======================================================================
@@ -471,7 +472,7 @@ contains
 
     ! Write out the source information
     do ns0=1,NumSrc
-       
+
        ! Content depends on what sources are being used
 #if defined(QUASARS) && defined(PL)
        format_str = "(3i4,2f10.3,1es15.3)"!,1e10.3)"
@@ -498,22 +499,22 @@ contains
        allocate(temparray(1))
        temparray(1) = SrcMass(ns0,0)
 #endif
-       
+
        ! Write the source position information
        write(49,format_str) srcpos(1,ns0),srcpos(2,ns0),srcpos(3,ns0), &
             temparray(1:size(temparray))
-       
+
     enddo
-    
+
     ! Close the file
     close(49)
-    
+
   end subroutine save_source_list
 
   ! =======================================================================
 
   subroutine assign_uv_luminosities (lifetime2,nz)
-    
+
     real(kind=dp),intent(in) :: lifetime2 ! time step
     integer,intent(in) :: nz
 
@@ -531,7 +532,7 @@ contains
 #endif
     endif
 #endif
-    
+
     ! Turn masses into luminosities
     select case (UV_Model)
 
@@ -603,18 +604,18 @@ contains
           if (rank == 0) write(logf,*) "No UV model available, setting fluxes to zero."
        endif
     end select
-    
+
 #ifdef MPILOG
     if (rank /=0) then
        write(logf,*) 'Assigned UV luminosities to sources'
        write(logf,*) 'UV_Model=',UV_Model
     endif
 #endif
-    
+
   end subroutine assign_uv_luminosities
-  
+
   ! =======================================================================
-  
+
   function Luminosity_from_mass (Mass)
 
     ! The normalized flux (luminosity) for normal sources is expressed
@@ -686,7 +687,7 @@ contains
     real(kind=dp) :: delta_E
     real(kind=dp) :: QPL_Luminosity_convert
     real(kind=dp) :: alpha
-    
+
     !Convert luminosity to total number of photons per second
     Emin = qpl_MinFreq_nominal/(ev2fr) !in ev
     Emax = qpl_MaxFreq_nominal/(ev2fr) !in ev
@@ -695,10 +696,10 @@ contains
     alpha = qpl_index_nominal - 1.0
 
     QPL_Luminosity_convert = -1.0/delta_E * Lum/(2000**(-alpha)) * &
-                1.0/alpha*(Emax**(-alpha) - Emin**(-alpha))
-!    write(*,*) "Emax, Emin: ", Emax, Emin    
-!    write(*,*) "Lum: ", Lum
-!    if (QPL_Luminosity_convert /= 0.0) write(*,*) "number of photons: ", QPL_Luminosity_convert
+         1.0/alpha*(Emax**(-alpha) - Emin**(-alpha))
+    !    write(*,*) "Emax, Emin: ", Emax, Emin    
+    !    write(*,*) "Lum: ", Lum
+    !    if (QPL_Luminosity_convert /= 0.0) write(*,*) "number of photons: ", QPL_Luminosity_convert
 
     !Normalise
     QPL_Luminosity_convert = QPL_Luminosity_convert/qpl_S_star_nominal
@@ -710,14 +711,14 @@ contains
   !> Initialization routine: determine the source model and optionally read 
   !! in source properties
   !! Author: Garrelt Mellema
-  
-  
+
+
   !! This accomodates different source models
   !! 0: Iliev et al source, Ndot_gamma= f*M_halo/timestep
   !! 1: Fixed total N_gamma, still need to divide by time step
   !! 2: Fixed total Ndot_gamma.
   subroutine source_properties_ini ()
-    
+
 
     integer :: uv_answer
     real(kind=dp) :: z_in, N_source_nosupp, N_source_supp, N_gamma_nosupp
@@ -736,11 +737,11 @@ contains
        case(2)
           UV_Model = "Fixed Ndot_gamma"
        end select
-       
+
        if (uv_answer > 0) then
           if (.not.file_input) write(*,"(A,$)") "File with UV data: "
           read(stdinput,*) uv_file
-          
+
           ! Open and read redshift file
           open(unit=60,file=uv_file,form="formatted",status="old")
           read(unit=60,fmt=*) NumZred_uv
@@ -774,16 +775,16 @@ contains
             mympierror)
     endif
 #endif
-    
+
   end subroutine source_properties_ini
 
   ! =======================================================================
-  
+
   subroutine create_random_source_list()
 
     ! For random permutation of sources
     use  m_ctrper
-    
+
     integer :: ns
 
     ! First deallocate the array (if it was allocated)
@@ -797,11 +798,11 @@ contains
        do ns=1,NumSrc
           SrcSeries(ns)=ns
        enddo
-       
+
        ! Make a random order
        call ctrper(SrcSeries(1:NumSrc),1.0)
     endif
-    
+
 #ifdef MPI
     ! Distribute the source series to the other nodes
     call MPI_BCAST(SrcSeries,NumSrc,MPI_INTEGER,0,MPI_COMM_NEW,mympierror)
